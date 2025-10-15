e05a4090fff5410a24832a9a4201eb33811cc602
Niño Raleigh Y. Paragas <ninoraleighparagas@gmail.com>
2025-10-15 15:50:02 +0800

    tehthftgjh

--- app/components/homepage/QuewieWay.vue
+++ app/components/homepage/QuewieWay.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="h-[689px] bg-gray-50 py-20 px-21.75 shadow-[0px_8px_7.3px_1px_#00000008]"
-        style="font-family: 'Poppins', sans-serif;">
-        <div class="h-[529px] bg-white py-[85.5px] px-[35.5px] rounded-[20px]">
-            <div class=" flex items-center  justify-between px-5 h-full">
+    <div class="h-[689px] bg-gray-50 py-20 px-21.75 shadow-[0px_8px_7.3px_1px_#00000008] font-sans">
+        <div class="h-132 bg-white py-21 px-9 rounded-2xl">
+            <div class=" flex items-center border justify-between  h-full">
                 <!-- left -->
                 <div class="w-1/2 bg-amber-200">
                     <div class="space-y-5">
-                        <h1 class="font-medium text-[25px] leading-[160%] w-[506px]">The QUEWIE Way: <span
+                        <h1 class="font-medium text-2xl leading-relaxed w-126">The QUEWIE Way: <span
                                 class="text-[#388FF0]"> Stress-Free Service, Happier Customers </span></h1>
-                        <p class="font-normal text-xl leading-[180%] tracking-[2%] text-[#696984] w-[563px]">QUEWIE
+                        <p class="font-normal text-xl leading-loose tracking-wide text-[#696984] w-140">QUEWIE
                             simplifies workflows, eases employee stress, and ensures faster, smoother service for
                             happier customers.</p>
                         <p
-                            class="font-normal text-[22px] leading-[180%] text-[#696984] underline decoration-solid decoration-[0px] underline-offset-1">
+                            class="font-normal text-xl leading-loose text-[#696984] underline  underline-offset-1">
                             Learn more</p>
                     </div>
                 </div>
 
                 <!-- right -->
-                <div class="w-1/2 bg-blue-300">
-                    <div></div>
-                    <img src="/images/Group 17.png">
+                <div class="w-1/2 h-full bg-blue-300 relative ">
+                    <div class="w-27 h-24 bg-[#272727] rounded-3xl absolute right-117"></div>
+                    <div class="w-45 h-41 bg-[#388FF0] rounded-3xl absolute right-0 bottom-0"></div>
+                    <img src="/images/Group 17.png" class="w-136 h-82 absolute right-4 top-1/2 -translate-y-1/2">
 
                 </div>
             </div>
